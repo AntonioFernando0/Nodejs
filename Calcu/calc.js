0db42f8fcdcc0af8bc1863418520d3958abcfea4
AntonioFernando0 <jufer376@gmail.com>
2024-07-31 21:30:41 +0100

    Módulos
    Módulos em Nodejs

---
+++ Calcu/calc.js
@@ -0,0 +1,11 @@
+let SomaFunc = require("./soma");
+let SubFunc =  require("./sub");
+let divFunc = require("./div");
+let multiFunc = require("./multi");
+
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(30,2));
+console.log(multiFunc(50,2));
+console.log(divFunc(100,2));
+
