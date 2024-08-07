aed2b1f0d3832ca755458baaafe85d6908966359
AntonioFernando0 <jufer376@gmail.com>
2024-08-07 16:17:20 +0100

    Express.js
    Express.js

--- Express/index.js
+++ Express/index.js
@@ -6,14 +6,41 @@ app.get("/", function(req, res){
 
 }); // Criando uma rota
 
-app.get("blog", function(req, res){
-    res.send("Bem vindo ao meu Blog!")
+app.get("/blog/:artigo?", function(req, res)// QUANDO QUISER COLOCAR UM PARAMENTRO OPCIONAL
+{
+
+    let artigo = req.params.artigo
+    if(artigo){
+        res.send("<h2> Artigo: " + artigo + " </h2>")
+
+    }else{
+        res.send("Bem vindo ao meu Blog!")
+
+    }
+    
 })
 
 app.get("/canal/youtube",function(req, res){
-    res.send("Bem vindo ao meu Canal")
+
+    let canal = req.query["canal"]
+    if(canal){
+        res.send(canal)
+    }else{ res.send("Nenhum canal fornecido!")
+
+    }
+   
 })
-app.listen(40, function(erro){
+
+app.get("/ola/:nome/:empresa", function(req, res){
+
+    // REQ => DADOS ENVIADOS PELO USUÁRIO
+    // RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUÁRIO
+    let nome = req.params.nome
+    let empresa = req.params.empresa
+    res.send("<h1>Oi "  +  nome  + " do " + empresa + " </h1>");
+})
+
+app.listen(4000, function(erro){
     if(erro){
 
         console.log("Ocorreu um erro!")
